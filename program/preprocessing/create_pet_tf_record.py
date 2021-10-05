75024eddca8818e6cfcd902f43c123a9cf0a984f
pallavi-pannu-oc <67274173+pallavi-pannu-oc@users.noreply.github.com>
2021-10-05 12:53:30 +0530

    Update create_pet_tf_record.py

--- program/preprocessing/create_pet_tf_record.py
+++ program/preprocessing/create_pet_tf_record.py
@@ -24,7 +24,7 @@ import xml.etree.ElementTree as ET
 import argparse
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    # Suppress TensorFlow logging (1)
-import tensorflow.compat.v1 as tf
+import tensorflow as tf
 from PIL import Image
 from object_detection.utils import dataset_util, label_map_util
 from collections import namedtuple
